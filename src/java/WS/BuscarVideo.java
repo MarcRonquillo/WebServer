113a28ff6a67296be5c601175f72613e24b69153
marcRonquillo <marc.ronq@gmail.com>
2016-04-21 19:10:15 +0200

    canvis en el metode de cerca

--- src/java/WS/BuscarVideo.java
+++ src/java/WS/BuscarVideo.java
@@ -6,8 +6,14 @@
 package WS;
 
 import hibernate.util.HibernateUtil;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -38,7 +44,7 @@ public class BuscarVideo {
             tx = session.getTransaction();
             tx.begin();
             Criteria criteria = session.createCriteria(Videos.class);
-
+            
             if (!autor.equals("")) {
                 criteria.add(Restrictions.ilike("autor", "%" + autor + "%"));
             }
@@ -54,4 +60,58 @@ public class BuscarVideo {
 
         return result;
     }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "buscarVideos")
+    public java.util.List<model.Videos> buscarVideos(@WebParam(name = "titulo") String titulo, @WebParam(name = "autor") String autor, @WebParam(name = "fecha") String fecha) {
+
+        java.util.Date fechaCreacion = null;
+        java.sql.Date fechaCreacionSql = null;
+        if (fecha != null) {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+            try {
+                fechaCreacion = (Date) df.parse(fecha);
+            } catch (ParseException ex) {
+                Logger.getLogger(BuscarVideo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            fechaCreacionSql = new java.sql.Date(fechaCreacion.getTime());
+        }
+        
+        
+        List<Videos> result = new ArrayList<Videos>();
+        Session session = HibernateUtil.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Criteria criteria = session.createCriteria(Videos.class);
+            
+            if (!titulo.equals("")) {
+                criteria.add(Restrictions.ilike("titulo", "%" + titulo + "%"));
+            }
+            if (!autor.equals("")) {
+                criteria.add(Restrictions.ilike("autor", "%" + autor + "%"));
+            }
+            if (fechaCreacionSql != null) {
+                criteria.add(Restrictions.eq("fechaCreacion", fechaCreacionSql));
+            }
+            result = criteria.list();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            System.out.println(e);
+        } finally {
+            session.close();
+        }
+
+        return result;
+    }
 }
+
+
+
+
